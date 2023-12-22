ef2c7b013f19a0db6d002a66f81f18b2ccff5906
s62079 <s62079@ocean.umt.edu.my>
2023-12-23 03:05:10 +0800

    Update model_task.dart

--- project1/lib/model_task.dart
+++ project1/lib/model_task.dart
@@ -2,11 +2,13 @@ class Task {
   final DateTime created;
   DateTime? deadline;
   String title;
-  bool done = false;
+  String? details;
+  bool done = false, priority = false;
 
   Task({
     required this.created,
     required this.title,
     this.deadline,
+    this.details,
   });
 }
