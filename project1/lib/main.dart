b800a78c2a5f2b179a9bfbd9d793129be0bec8d2
s62079 <s62079@ocean.umt.edu.my>
2023-12-23 03:04:58 +0800

    Update main.dart

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -13,10 +13,8 @@ class TaskApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
       home: const ViewTasks(),
     );
   }
